b91e53e9b567c751437137f9ac63820a2fc93d8a
Tomas Martinez <tomashmartinez@javeriana.edu.co>
2023-04-18 15:59:28 -0500

    Se arreglo reservas

--- back/src/main/java/com/web/back/DTO/ReservaDTO.java
+++ back/src/main/java/com/web/back/DTO/ReservaDTO.java
@@ -1,5 +1,6 @@
 package com.web.back.DTO;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,7 +8,9 @@ import java.time.LocalDateTime;
 
 @Data
 public class ReservaDTO {
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime fechaInicio;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime fechaFin;
     private int usuarioId;
     private int canchaId;
