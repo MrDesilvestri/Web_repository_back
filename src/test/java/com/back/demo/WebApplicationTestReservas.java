3c382f03bdd8c2c08bd6e7b6a2b0020cb58403e8
Tomas Martinez <tomashmartinez@javeriana.edu.co>
2023-04-25 13:57:12 -0500

    Se agrego una parte de las pruebas unitarias al controlador de canchas

---
+++ src/test/java/com/back/demo/WebApplicationTestReservas.java
@@ -0,0 +1,16 @@
+package com.back.demo;
+import com.back.demo.Controllers.UsuarioController;
+import com.back.demo.Model.Usuario;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTestReservas {
+
+    @Test
+    @Autowired
+    void Test1() throws Exception {
+
+    }
+}
